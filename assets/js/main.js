80272096c8c8adf563a34ec047a41f62023f6f72
CipiVlad <supercoder.cipi@gmail.com>
2022-04-13 23:40:25 +0200

    js_comments

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+// This is a single line JavaScript comment
+document.write('<h2>I have comments in my JavaScript code!</h2>');
+// document.write("You can't see this!");
+//-->
+
+document.write("<h3>I have multi-line comments!</h3>");
+
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
